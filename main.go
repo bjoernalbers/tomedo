d202fd87598e98a881e66746fe37cef4cc5152ad
Björn Albers <bjoernalbers@gmail.com>
2022-08-17 11:10:52 +0200

    Display tomedo users.

--- main.go
+++ main.go
@@ -10,20 +10,24 @@ import (
 )
 
 func main() {
-	/*
-			query := "select kuerzel,vorname,nachname from nutzer where visible = true and kuerzel not in ('test','zollsoft','admin') order by kuerzel"
-	    uri := "postgresql://zollsoft_readonly:tomedo@tomedo/tomedo"
-			cmd := exec.Command("psql", "--command", query, "--tuples-only", "--csv", uri)
-			output, err := cmd.Output()
-			fmt.Print(string(output))
-			if err != nil {
-				fmt.Println(err)
-			}
-	*/
 	uri := "postgresql://zollsoft_readonly:tomedo@tomedo/tomedo"
 	db, err := sql.Open("postgres", uri)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(db.Ping())
+
+	query := "select kuerzel,vorname,nachname from nutzer where visible = true and kuerzel not in ('test','zollsoft','admin') order by kuerzel"
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	var kuerzel, vorname, nachname string
+	for rows.Next() {
+		rows.Scan(&kuerzel, &vorname, &nachname)
+		fmt.Println(kuerzel, vorname, nachname)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
