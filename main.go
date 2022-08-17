9b40e8110f1ab687e8bb9ded4fe6d15b7b6cfedf
Björn Albers <bjoernalbers@gmail.com>
2022-08-17 10:42:34 +0200

    Test connection via sql library.

--- main.go
+++ main.go
@@ -2,17 +2,28 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"os/exec"
+	"log"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	query := "select kuerzel,vorname,nachname from nutzer where visible = true and kuerzel not in ('test','zollsoft','admin') order by kuerzel"
+	/*
+			query := "select kuerzel,vorname,nachname from nutzer where visible = true and kuerzel not in ('test','zollsoft','admin') order by kuerzel"
+	    uri := "postgresql://zollsoft_readonly:tomedo@tomedo/tomedo"
+			cmd := exec.Command("psql", "--command", query, "--tuples-only", "--csv", uri)
+			output, err := cmd.Output()
+			fmt.Print(string(output))
+			if err != nil {
+				fmt.Println(err)
+			}
+	*/
 	uri := "postgresql://zollsoft_readonly:tomedo@tomedo/tomedo"
-	cmd := exec.Command("psql", "--command", query, "--tuples-only", "--csv", uri)
-	output, err := cmd.Output()
-	fmt.Print(string(output))
+	db, err := sql.Open("postgres", uri)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
+	fmt.Println(db.Ping())
 }
